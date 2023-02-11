49b7ad85101c1f152fb19ac8f2dd4b614d589c8c
Romzik-Peperomzik <warriorway5657@gmail.com>
2023-02-11 17:43:20 +0300

    fixed auth feedback components/LoginPage.jsx

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useFormik } from 'formik';
 import React, { useEffect, useRef, useState } from 'react';
 import {
-  Button, Form, Container, Row, Col, Card, Image, FloatingLabel,
+  Button, Form, Container, Row, Col, Card, Image,
 } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -36,16 +36,17 @@ const LoginPage = () => {
       password: yup.string()
         .required(t('feedback.validationRequired')),
     }),
-    onSubmit: async (values) => {
-      setAuthFailed(false);
-
+    onSubmit: async (values, actions) => {
+      actions.setSubmitting(true);
       try {
         const userData = await auth.authorizeUser(routes.loginPath(), values);
+        actions.setSubmitting(false);
+        setAuthFailed(false);
         navigate({ pathname: '/' });
         rollbar.info(`${userData.username} logged in`);
       } catch (err) {
         console.log(err);
-        formik.setSubmitting(false);
+        actions.setSubmitting(false);
         setAuthFailed(true);
         if (err.code === 'ERR_NETWORK') toast.error(t('feedback.noNetwork'));
         if (err.isAxiosError && err.response.status === 401) inputRef.current.select();
@@ -58,6 +59,7 @@ const LoginPage = () => {
       <Row className="justify-content-center align-content-center h-100">
         <Col sm={12} md={8} lg={8} xxl={6}>
           <Card className="shadow-sm">
+
             <Card.Body as={Row} className="p-5">
               <Col sm={12} md={6} className="d-flex align-items-center justify-content-center">
                 <Image src={imgLogin} className="rounded-circle" />
@@ -66,41 +68,47 @@ const LoginPage = () => {
                 <h1 className="text-center mb-4">{t('forms.loginHeader')}</h1>
                 <fieldset disabled={formik.isSubmitting}>
                   <Form.Group className="form-floating mb-3">
-                    <FloatingLabel htmlFor="username" label={t('forms.usernickLabel')}>
-                      <Form.Control
-                        onChange={formik.handleChange}
-                        value={formik.values.username}
-                        placeholder={t('forms.usernickLabel')}
-                        name="username"
-                        id="username"
-                        autoComplete="username"
-                        isInvalid={authFailed}
-                        required
-                        ref={inputRef}
-                      />
-                    </FloatingLabel>
+                    <Form.Control
+                      onChange={formik.handleChange}
+                      value={formik.values.username}
+                      placeholder={t('forms.usernickLabel')}
+                      name="username"
+                      id="username"
+                      autoComplete="username"
+                      isInvalid={authFailed}
+                      required
+                      ref={inputRef}
+                    />
+                    <Form.Label htmlFor="username">{t('forms.usernickLabel')}</Form.Label>
                   </Form.Group>
 
                   <Form.Group className="form-floating mb-4">
-                    <FloatingLabel htmlFor="password" label={t('forms.passwordLabel')}>
-                      <Form.Control
-                        type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        placeholder="password"
-                        name="password"
-                        id="password"
-                        autoComplete={t('forms.passwordLabel')}
-                        isInvalid={authFailed}
-                        required
-                      />
-                      <Form.Control.Feedback type="invalid" tooltip>{t('feedback.invalidLoginAttempt')}</Form.Control.Feedback>
-                    </FloatingLabel>
+                    <Form.Control
+                      type="password"
+                      onChange={formik.handleChange}
+                      placeholder={t('forms.passwordLabel')}
+                      name="password"
+                      id="password"
+                      autoComplete="password"
+                      isInvalid={authFailed}
+                      required
+                    />
+                    <Form.Label htmlFor="password">{t('forms.passwordLabel')}</Form.Label>
+
+                    {authFailed
+                      ? (
+                        <Form.Control.Feedback type="invalid" tooltip>
+                          {t('feedback.invalidLoginAttempt')}
+                        </Form.Control.Feedback>
+                      ) : null}
                   </Form.Group>
-                  <Button type="submit" variant="outline-primary" className="w-100 mb-3">{t('controls.loginButton')}</Button>
+                  <Button type="submit" variant="outline-primary" className="w-100 mb-3" disabled={formik.isSubmitting}>
+                    {t('controls.loginButton')}
+                  </Button>
                 </fieldset>
               </Col>
             </Card.Body>
+
             <Card.Footer className="p-4">
               <div className="text-center">
                 <span>{t('forms.loginFooterNoAcc')}</span>
