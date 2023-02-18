ce24afa2efebb6a6199b56b53ff7f19c907d72c2
Romzik-Peperomzik <warriorway5657@gmail.com>
2023-02-18 08:51:13 +0300

    refactored hoc/SocketApiProvider.jsx

--- frontend/src/hoc/SocketApiProvider.jsx
+++ frontend/src/hoc/SocketApiProvider.jsx
@@ -5,50 +5,47 @@ import store from '../slices/index';
 import { addChannel, renameChannel, removeChannel } from '../slices/channelsSlice';
 import { addMessage } from '../slices/messagesSlice';
 
+const socketApiHandler = (socket) => ({
+  newMessage(data) {
+    socket.emit('newMessage', data, (response) => {
+      if (response.status === 'ok') return;
+      throw new Error(response.err);
+    });
+  },
+  newChannel(data, callbackHandler) {
+    socket.emit('newChannel', data, (response) => {
+      callbackHandler(response);
+    });
+  },
+  renameChannel(data, callbackHandler) {
+    socket.emit('renameChannel', data, (response) => {
+      callbackHandler(response);
+    });
+  },
+  removeChannel(data, callbackHandler) {
+    socket.emit('removeChannel', data, (response) => {
+      callbackHandler(response);
+    });
+  },
+});
+
 const SocketApiProvider = ({ children, socket }) => {
-  const socketApiHandler = () => {
-    socket.on('newMessage', (message) => {
-      store.dispatch(addMessage(message));
-    });
+  socket.on('newMessage', (message) => {
+    store.dispatch(addMessage(message));
+  });
 
-    socket.on('newChannel', (channel) => {
-      store.dispatch(addChannel(channel));
-    });
+  socket.on('newChannel', (channel) => {
+    store.dispatch(addChannel(channel));
+  });
 
-    socket.on('renameChannel', ({ id, name }) => {
-      store.dispatch(renameChannel({ id, changes: { name } }));
-    });
+  socket.on('renameChannel', ({ id, name }) => {
+    store.dispatch(renameChannel({ id, changes: { name } }));
+  });
 
-    socket.on('removeChannel', ({ id }) => {
-      store.dispatch(removeChannel(id));
-    });
-
-    const socketApi = {
-      newMessage(data) {
-        socket.emit('newMessage', data, (response) => {
-          if (response.status === 'ok') return;
-          throw new Error(response.err);
-        });
-      },
-      newChannel(data, callbackHandler) {
-        socket.emit('newChannel', data, (response) => {
-          callbackHandler(response);
-        });
-      },
-      renameChannel(data, callbackHandler) {
-        socket.emit('renameChannel', data, (response) => {
-          callbackHandler(response);
-        });
-      },
-      removeChannel(data, callbackHandler) {
-        socket.emit('removeChannel', data, (response) => {
-          callbackHandler(response);
-        });
-      },
-    };
-    return socketApi;
-  };
-  const socketApi = socketApiHandler();
+  socket.on('removeChannel', ({ id }) => {
+    store.dispatch(removeChannel(id));
+  });
+  const socketApi = socketApiHandler(socket);
 
   return (
     <SocketApiContext.Provider value={socketApi}>
